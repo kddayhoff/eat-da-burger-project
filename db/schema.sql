5d61bc009d673ae382bc43861048cce83ba39c6f
Karla Dayhoff <kddayhoff@gmail.com>
2020-05-04 22:01:07 -0500

    created all files and directories, completed schema and seed files

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_names VARCHAR(255),
+    devoured BOOLEAN,
+    primary key (id)
+);
+   
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_names, devoured) VALUES ("Mushroom burger", true);
+
+INSERT INTO burgers (burger_names, devoured) VALUES ("Bacon cheeseburger", true);
+
+INSERT INTO burgers (burger_names, devoured) VALUES ("Patty Melt", false);
+
+INSERT INTO burgers (burger_names, devoured) VALUES ("Veggie burger", false);
