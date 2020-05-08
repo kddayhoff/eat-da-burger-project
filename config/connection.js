26e97b1ec3c95521137a64445d5b5cc14483bfee
Karla Dayhoff <kddayhoff@gmail.com>
2020-05-08 00:29:37 -0500

    making heroku work

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,18 @@ var mysql = require("mysql");
 var dotenv = require("dotenv");
 var env = require("../.env");
 
-var connection = mysql.createConnection({
+var connection;
+if(process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+ } else { 
+   connection = mysql.createConnection({
   host: process.env.DB_HOST,
   port: 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: "burgers_db"
-});
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
